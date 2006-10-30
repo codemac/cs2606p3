d74805ecc7551509aafa0a27b64e832adbeba1c7
codemac <codemac@5d240175-8320-0410-806b-6be057f0743d>
2006-10-30 02:46:57 +0000

    doc'd actions for getNextBuffer

--- trunk/BufferPool.cpp
+++ trunk/BufferPool.cpp
@@ -5,6 +5,12 @@ BufferPool::insert(string str, void* location) {
 }
 
 BufferPool::getNextBuffer(void) {
+	if ( count == total ) {
+		count = 0;
+		//	reset count
+		//	write top buffer to disk
+		//	reuse old memory
+	}
 	return memory[(count++)%total];
 }
 
