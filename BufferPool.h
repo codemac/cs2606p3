c36af1ddeee6ae01ca2b300c60fe46994cc35815
codemac <codemac@5d240175-8320-0410-806b-6be057f0743d>
2006-10-30 02:01:20 +0000

    making memory a char***

--- BufferPool.h
+++ BufferPool.h
@@ -1,22 +1,27 @@
 #ifndef BUFFERPOOL_H
 #define BUFFERPOOL_H
 
-#include <stdio>
+#include <cstdio>
 #include <string>
 #include <fstream>
+#include "Handle.h"
 
 #define BLOCKSIZE 512
 
+using namespace std;
+
 class BufferPool {
 	private:
-		string file;
 		fstream stream;
+		char*** memory;
 	public:
 		BufferPool();
-		BufferPool(string theFile)
-			:file (theFile),
-			stream(fstream(theFile, ios::binary | ios::out | ios::in))
-			{}
+		BufferPool(string theFile, int numBuffs) {
+			stream = fstream(theFile, ios::binary | ios::out | ios::in);
+			memory = new char[numBuffs][BLOCKSIZE];
+		}
+
 		int insert(string str, int id, void* location);
+
 };
 #endif
