d8fe7167a2317d23d316ae698043800f6cbb8af2
bbraunduin <bbraunduin@5d240175-8320-0410-806b-6be057f0743d>
2006-10-30 02:43:54 +0000

    Updates so far.

--- MemoryManager.cpp
+++ MemoryManager.cpp
@@ -1,6 +1,20 @@
 #include "MemoryManager.h"
 
-MemoryManager::MemoryManager(int ID, string name)
+MemoryManager::MemoryManager()
+{
+    //BufferPool buffer;
+}
+
+MemoryManager::~MemoryManager()
 {
     
 }
+
+Handle MemoryManager::insert(int ID, string name)
+{
+    int size = name.size() + 4;
+    //search the linked list and add it in.
+    
+    
+}
+
