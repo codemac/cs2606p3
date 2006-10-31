c182436e57bec47c81be7225aa6aa82c67866f8b
codemac <codemac@5d240175-8320-0410-806b-6be057f0743d>
2006-10-31 03:17:39 +0000

    Buffer yum yum

--- trunk/BufferPool.cpp
+++ trunk/BufferPool.cpp
@@ -1,18 +1,3 @@
 #include "BufferPool.h"
 
-int BufferPool::insert(string str, int* location) {
-	
-}
-
-int* BufferPool::getNextBuffer() {
-	if ( count == total ) {
-		count = 0;
-		//	reset count
-		//	write top buffer to disk
-		//	reuse old memory
-	}
-	//	return memory[(count++)%total];
-	return 0;
-}
-
 
