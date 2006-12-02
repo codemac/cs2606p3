8a46f6799b72952aaf1e2a52edeff1f2229d8800
codemac <codemac@5d240175-8320-0410-806b-6be057f0743d>
2006-12-02 22:48:53 +0000

    moved btree.cpp, and templatiified btree.h

--- BTree.h
+++ BTree.h
@@ -1,9 +1,21 @@
 #ifndef BTREE_H
 #define BTREE_H
 
+#include "Record.h"
+
+using namespace std;
+
+template <typename R>
 class BTree {
 	private:
 	public:
+		bool insert(R* record);
+		bool delete(R* record);
+		void search(R* record, bool debug = true);
+		void searchRange(R* record1, R* record2, bool debug = true);
+		void dump();
 };
+
+#include "BTreeImplementation.h"
 
 #endif
