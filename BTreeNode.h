63d715069545317cc1e369133827535606c0c007
codemac <codemac@5d240175-8320-0410-806b-6be057f0743d>
2006-12-02 22:50:59 +0000

    added notes

---
+++ BTreeNode.h
@@ -0,0 +1,8 @@
+//	For internal nodes:
+//
+//	they need to store:
+//		Array of key/pointer pairs. 
+//		Pointer is the block of the child node. 
+//		Flag if internal node.
+//		Block number.
+//		How many children.
